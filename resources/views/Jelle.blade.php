e189e93fe7eb671b9c2358ac53ee6a68e425606a
Jelle van Oijen <ps228934@summacollege.nl>
2023-03-30 14:04:42 +0200

    added DaisyUI

--- resources/views/Jelle.blade.php
+++ resources/views/Jelle.blade.php
@@ -1,4 +1,10 @@
 <x-guest-layout>
     <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
     <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+    <div class="alert shadow-lg bg-red-500">
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>12 unread messages. Tap to see.</span>
+        </div>
+    </div>
 </x-guest-layout>
